6721826b0a2448890533752238604519a3913c25
Sidharth <sidharthpunathil714@gmail.com>
2021-01-03 23:36:49 +0530

    minor tweaks

--- bot.py
+++ bot.py
@@ -4,17 +4,19 @@ from bs4 import BeautifulSoup
 import time
 import re
 
+# Add additional delay to the TIME variable if your internet speed is low. 
 TIME = 0
 
 class KickStarterBot:
     def __init__(self):
         self.driver = webdriver.Edge(executable_path="edgedriver_win64\\msedgedriver.exe")
+        self.count = 0
         self.page = 1
         self.title = None
         self.author_name = None
         self.project_launch_date = None
         self.project_successfull_date = None
-        self.project_fund = None
+        self.project_successfull_fund = None
         self.project_location = None
         self.category = None
         self.location = None
@@ -32,32 +34,40 @@ class KickStarterBot:
         links = []
         self.driver.get("https://www.kickstarter.com/discover/advanced?woe_id=0&sort=magic&ref=discovery_overlay&seed=2682539&page={}".format(self.page))
         time.sleep(TIME+4)
-        current_page_links = self.driver.find_elements_by_xpath("//div[@class='clamp-5 navy-500 mb3 hover-target']/a")
+        if(self.driver.title != "The page you were looking for doesn't exist (404)"):
+            current_page_links = self.driver.find_elements_by_xpath("//div[@class='clamp-5 navy-500 mb3 hover-target']/a")
 
-        for each in current_page_links:
-            links.append(each.get_attribute("href"))
+            for each in current_page_links:
+                links.append(each.get_attribute("href"))
 
-        while(len(links) > 0):
-            current_link = links[0]
-            self.parse(current_link)
-            links.remove(current_link)
+            while(len(links) > 0):
+                current_link = links[0]
+                self.count += 1
+                self.parse(current_link)
+                links.remove(current_link)
 
-        self.page+=1
-        self.discover()
+            self.page+=1
+            self.discover()
+        else:
+            print("You have ${self.count} of project data in your database :)")
 
     def parse_community_section(self, link):
         """
         Parse the author name
         """
-        self.author_name = ''
-        self.driver.find_element_by_xpath("//*[@id='community-emoji']").click() # Go to Community tab
-        time.sleep(TIME+3)
-        sentence_with_name = self.driver.find_element_by_xpath('//div[@class="title"]').text
-        name_tuple = sentence_with_name.split(' ')[4:]
-        self.author_name = ' '.join(name_tuple)
 
-        if(self.author_name == None):
-            self.parse_community_section(link)
+        try:
+            self.driver.find_element_by_xpath("//*[@id='community-emoji']").click() # Go to Community tab
+            time.sleep(TIME+3)
+            sentence_with_name = self.driver.find_element_by_xpath('//div[@class="title"]').text
+            name_tuple = sentence_with_name.split(' ')[4:]
+            self.author_name = ' '.join(name_tuple)
+        except:
+            print("parse_community_section exception.")
+
+            if(self.author_name == None):
+                time.sleep(1)
+                self.parse_community_section(link)
 
         return self.author_name
 
@@ -67,27 +77,32 @@ class KickStarterBot:
         Parse Project launch date, Project fund and Project funds successfully date
         """
 
-        self.driver.find_element_by_xpath("//*[@id='updates-emoji']").click() # Go to Updates section
-        time.sleep(TIME+3)
-        
-        all_divs_in_current_page = self.driver.find_elements_by_tag_name('div')
-        for index, each in enumerate(all_divs_in_current_page):
-            if(each.text=="Project launches"):
-                self.project_launch_date = all_divs_in_current_page[index+1].text
-            elif(each.text=="Project funds successfully"):
-                self.project_fund = all_divs_in_current_page[index+1].text.split('$')[-1:]
-                self.project_successfull_date = all_divs_in_current_page[index+2].text
-                self.project_status = "successfull"                
+        try:
+            self.driver.find_element_by_xpath("//*[@id='updates-emoji']").click() # Go to Updates section
+            time.sleep(TIME+2)
+            
+            all_divs_in_current_page = self.driver.find_elements_by_tag_name('div')
+            for index, each in enumerate(all_divs_in_current_page):
+                if(each.text=="Project launches"):
+                    self.project_launch_date = all_divs_in_current_page[index+1].text
+                if(each.text=="Project funds successfully"):
+                    self.project_successfull_fund = all_divs_in_current_page[index+1].text.split('$')[-1:]
+                    self.project_successfull_date = all_divs_in_current_page[index+2].text
+                    self.project_status = "successfull"                
+        except:
+            print("parse_update_section exception.")
 
         if(self.project_launch_date == None):
+            time.sleep(1)
             self.parse_update_section(link)
 
-        return (self.project_launch_date, self.project_fund, self.project_successfull_date)
+        return (self.project_launch_date, self.project_successfull_fund, self.project_successfull_date)
 
     def parse_title(self, link):
         """
         Parse project title
         """
+
         page_source = self.driver.page_source
         soup = BeautifulSoup(page_source, 'lxml')
         self.title = soup.find_all('h2')[0].text
@@ -98,7 +113,7 @@ class KickStarterBot:
         """
             Parse project location and category
         """
-
+        
         tag_bar = self.driver.find_elements_by_xpath("//div[@class='py2 py3-lg flex items-center auto-scroll-x']")[0]
         tags = tag_bar.text.split('\n')
 
@@ -117,7 +132,9 @@ class KickStarterBot:
     def go_to_project(self, link):
         """
         Go to a project page
+        
         """
+        
         self.driver.get(link)
         time.sleep(TIME + 4)
 
@@ -125,11 +142,21 @@ class KickStarterBot:
         """
         Parse goal, raised and backers
         """
+        
         raised_item = self.driver.find_elements_by_xpath('//span[@class="ksr-green-500"]')[0].text
-        self.raised = raised_item.split(' ')[1]
+        self.raised = raised_item.split('$')[1]
+
         goal_item = self.driver.find_elements_by_xpath("//span[@class='block dark-grey-500 type-12 type-14-md lh3-lg']")[0].text
+        print(goal_item)
         self.goal = goal_item.split(' ')[3]
-        self.backers = self.driver.find_elements_by_xpath('//div[@class="block type-16 type-28-md bold dark-grey-500"]/span')[0].text
+        self.backers = self.driver.find_elements_by_xpath('//div[@class="block type-16 type-28-md bold dark-grey-500"]/span')[0].text            
+        if(self.goal=="goal"):
+            self.goal = goal_item.split(' ')[2]
+
+        if(self.goal[:1]!='$'):
+            self.goal = '${}'.format(self.goal)
+        if(self.raised[1:]!='$'):
+            self.raised = '${}'.format(self.raised)
 
         return (self.goal, self.raised, self.backers)
         
@@ -137,12 +164,27 @@ class KickStarterBot:
         """
         Parsing for details. 
         """
+        print("Count: ", self.count)
         self.go_to_project(link)
-        #self.parse_title(link)
-        #self.parse_update_section(link)
-        #self.parse_community_section(link)
-        #self.parse_project_loc_cat(link)
+        print("URL: ", link)
+        self.parse_title(link)
+        print("Title: ", self.title)
         self.goal_and_raised_backers(link)
+        print("Goal: ", self.goal)
+        print("Raised: ", self.raised)
+        print("Backers: ", self.backers)
+        self.parse_project_loc_cat(link)
+        print("Location: ", self.location)
+        print("Category: ", self.category)
+        self.parse_update_section(link)
+        print("Status: ", self.project_status)
+        if(self.project_status!="inprogress"):
+            print("Project successfull Fund: ", self.project_successfull_fund)
+        print("Launch: ", self.project_launch_date)
+        if(self.project_status!="inprogress"):
+            print("Project Status: ", self.project_successfull_date)
+        self.parse_community_section(link)
+        print("Author : ", self.author_name)
 
         payload = {
             'url': link,
@@ -150,7 +192,7 @@ class KickStarterBot:
             'auther': self.author_name,
             'launched': self.project_launch_date,
             'successfull_date': self.project_successfull_date,
-            'fund': self.project_fund,
+            'successfull_fund': self.project_successfull_fund,
             'status': self.project_status,
             'location': self.project_location,
             'category': self.category,
